dcfa64611840294ef56e7b511d020eaa2c3fdda6
Kireeva <kireeva.en@defo.ru>
2021-07-22 17:51:32 +0300

    inishial

---
+++ js/script.js
@@ -0,0 +1,227 @@
+// ------------- VARIABLES ------------- //
+var ticking = false;
+var isFirefox = (/Firefox/i.test(navigator.userAgent));
+var isIe = (/MSIE/i.test(navigator.userAgent)) || (/Trident.*rv\:11\./i.test(navigator.userAgent));
+var scrollSensitivitySetting = 30; //Increase/decrease this number to change sensitivity to trackpad gestures (up = less sensitive; down = more sensitive) 
+var slideDurationSetting = 600; //Amount of time for which slide is "locked"
+var currentSlideNumber = 0;
+var totalSlideNumber = $(".background").length;
+
+// ------------- DETERMINE DELTA/SCROLL DIRECTION ------------- //
+function parallaxScroll(evt) {
+  if (isFirefox) {
+    //Set delta for Firefox
+    delta = evt.detail * (-120);
+  } else if (isIe) {
+    //Set delta for IE
+    delta = -evt.deltaY;
+  } else {
+    //Set delta for all other browsers
+    delta = evt.wheelDelta;
+  }
+
+  if (ticking != true) {
+    if (delta <= -scrollSensitivitySetting) {
+      //Down scroll
+      ticking = true;
+      if (currentSlideNumber !== totalSlideNumber - 1) {
+        currentSlideNumber++;
+        nextItem();
+      }
+      slideDurationTimeout(slideDurationSetting);
+    }
+    if (delta >= scrollSensitivitySetting) {
+      //Up scroll
+      ticking = true;
+      if (currentSlideNumber !== 0) {
+        currentSlideNumber--;
+      }
+      previousItem();
+      slideDurationTimeout(slideDurationSetting);
+    }
+  }
+}
+
+// ------------- SET TIMEOUT TO TEMPORARILY "LOCK" SLIDES ------------- //
+function slideDurationTimeout(slideDuration) {
+  setTimeout(function() {
+    ticking = false;
+  }, slideDuration);
+}
+
+// ------------- ADD EVENT LISTENER ------------- //
+var mousewheelEvent = isFirefox ? "DOMMouseScroll" : "wheel";
+window.addEventListener(mousewheelEvent, _.throttle(parallaxScroll, 60), false);
+
+// ------------- SLIDE MOTION ------------- //
+function nextItem() {
+  var $previousSlide = $(".background").eq(currentSlideNumber - 1);
+  $previousSlide.removeClass("up-scroll").addClass("down-scroll");
+}
+
+function previousItem() {
+  var $currentSlide = $(".background").eq(currentSlideNumber);
+  $currentSlide.removeClass("down-scroll").addClass("up-scroll");
+}
+
+
+
+
+// =======================================================================>
+/* ---- particles.js config ---- */
+
+particlesJS("particles-js", {
+	"particles": {
+	  "number": {
+		"value": 380,
+		"density": {
+		  "enable": true,
+		  "value_area": 2000
+		}
+	  },
+	  "color": {
+		// "value": "#ffffff"
+		"value": "#4DBBC5"
+	  },
+	  "shape": {
+		"type": "circle",
+		"stroke": {
+		  "width": 0,
+		  "color": "#000000"
+		},
+		"polygon": {
+		  "nb_sides": 5
+		},
+		"image": {
+		  "src": "img/github.svg",
+		  "width": 100,
+		  "height": 100
+		}
+	  },
+	  "opacity": {
+		"value": 0.6,
+		"random": false,
+		"anim": {
+		  "enable": false,
+		  "speed": 1,
+		  "opacity_min": 0.1,
+		  "sync": false
+		}
+	  },
+	  "size": {
+		"value": 4,
+		"random": true,
+		"anim": {
+		  "enable": false,
+		  "speed": 40,
+		  "size_min": 0.1,
+		  "sync": false
+		}
+	  },
+	  "line_linked": {
+		"enable": true,
+		"distance": 150,
+		"color": "#ffffff",
+		"opacity": 0.4,
+		"width": 1
+	  },
+	  "move": {
+		"enable": true,
+		"speed": 5,
+		"direction": "none",
+		"random": false,
+		"straight": false,
+		"out_mode": "out",
+		"bounce": false,
+		"attract": {
+		  "enable": false,
+		  "rotateX": 600,
+		  "rotateY": 1200
+		}
+	  }
+	},
+	"interactivity": {
+	  "detect_on": "canvas",
+	  "events": {
+		"onhover": {
+		  "enable": true,
+		  "mode": "grab"
+		},
+		"onclick": {
+		  "enable": true,
+		  "mode": "push"
+		},
+		"resize": true
+	  },
+	  "modes": {
+		"grab": {
+		  "distance": 200,
+		  "line_linked": {
+			"opacity": 1
+		  }
+		},
+		"bubble": {
+		  "distance": 400,
+		  "size": 40,
+		  "duration": 2,
+		  "opacity": 8,
+		  "speed": 3
+		},
+		"repulse": {
+		  "distance": 200,
+		  "duration": 0.4
+		},
+		"push": {
+		  "particles_nb": 4
+		},
+		"remove": {
+		  "particles_nb": 2
+		}
+	  }
+	},
+	"retina_detect": true
+  });
+  
+  
+
+//   ====================================================>
+
+/* Demo purposes only */
+$(".hover").mouseleave(
+	function() {
+	  $(this).removeClass("hover");
+	}
+  );
+
+
+
+
+
+//   ========================================================================>
+
+// Modal Window
+(() => {
+	const modalBtns = Array.from(document.querySelectorAll(".otherPages"));
+	modalBtns.forEach(btn => {
+	  btn.onclick = function() {
+		const modal = btn.getAttribute('data-modal');
+		document.getElementById(modal).style.display = "block";
+		document.querySelector("body").style.overflow = 'hidden';
+	  }
+	});
+	
+	const closeBtns = Array.from(document.querySelectorAll(".close-button"));
+	closeBtns.forEach(btn => {
+	  btn.onclick = function() {
+		let modal = btn.closest('.modal');
+		modal.style.display = "none";
+		document.querySelector("body").style.overflow = 'visible';
+	  }
+	});
+	
+	window.onclick = function(event) {
+	  if (event.target.className === "modal") {
+		event.target.style.display = "none";
+	  }
+	}
+	})();
